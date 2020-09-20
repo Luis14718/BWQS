88b58ec386c7348e46f8af3c8c245fb8229cc7a9
Luis Daniel Rodriguez Sanchez <43802987+Luis14718@users.noreply.github.com>
2020-09-20 14:39:23 -0600

    Draft key dispatcher

---
+++ Key Dispatcher/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Key_Dispatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDatos = new System.Windows.Forms.Button();
+            this.btnKey = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelleft = new System.Windows.Forms.Panel();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Iinicio = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.Iinicio.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDatos);
+            this.panel1.Controls.Add(this.btnKey);
+            this.panel1.Controls.Add(this.btnInicio);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(166, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDatos
+            // 
+            this.btnDatos.FlatAppearance.BorderSize = 0;
+            this.btnDatos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDatos.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDatos.ForeColor = System.Drawing.Color.White;
+            this.btnDatos.Image = global::Key_Dispatcher.Properties.Resources.tabla;
+            this.btnDatos.Location = new System.Drawing.Point(0, 288);
+            this.btnDatos.Name = "btnDatos";
+            this.btnDatos.Size = new System.Drawing.Size(163, 75);
+            this.btnDatos.TabIndex = 4;
+            this.btnDatos.Text = "Datos";
+            this.btnDatos.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDatos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnDatos.UseVisualStyleBackColor = true;
+            this.btnDatos.Click += new System.EventHandler(this.btnDatos_Click);
+            // 
+            // btnKey
+            // 
+            this.btnKey.FlatAppearance.BorderSize = 0;
+            this.btnKey.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKey.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKey.ForeColor = System.Drawing.Color.White;
+            this.btnKey.Image = global::Key_Dispatcher.Properties.Resources.en_uso;
+            this.btnKey.Location = new System.Drawing.Point(0, 207);
+            this.btnKey.Name = "btnKey";
+            this.btnKey.Size = new System.Drawing.Size(163, 75);
+            this.btnKey.TabIndex = 3;
+            this.btnKey.Text = "En uso";
+            this.btnKey.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnKey.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnKey.UseVisualStyleBackColor = true;
+            this.btnKey.Click += new System.EventHandler(this.btnKey_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft PhagsPa", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.ForeColor = System.Drawing.Color.White;
+            this.btnInicio.Image = global::Key_Dispatcher.Properties.Resources.inicio;
+            this.btnInicio.Location = new System.Drawing.Point(0, 126);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(163, 75);
+            this.btnInicio.TabIndex = 2;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnInicio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(166, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panelleft
+            // 
+            this.panelleft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panelleft.Location = new System.Drawing.Point(166, 126);
+            this.panelleft.Name = "panelleft";
+            this.panelleft.Size = new System.Drawing.Size(10, 75);
+            this.panelleft.TabIndex = 5;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Key_Dispatcher.Properties.Resources.logo_rancho;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(238, 71);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Key_Dispatcher.Properties.Resources.close;
+            this.pictureBox2.Location = new System.Drawing.Point(768, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 23);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Iinicio
+            // 
+            this.Iinicio.BackgroundImage = global::Key_Dispatcher.Properties.Resources.background;
+            this.Iinicio.Controls.Add(this.pictureBox4);
+            this.Iinicio.Controls.Add(this.label1);
+            this.Iinicio.Controls.Add(this.pictureBox1);
+            this.Iinicio.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Iinicio.Location = new System.Drawing.Point(166, 89);
+            this.Iinicio.Name = "Iinicio";
+            this.Iinicio.Size = new System.Drawing.Size(634, 361);
+            this.Iinicio.TabIndex = 1;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Key_Dispatcher.Properties.Resources.card1;
+            this.pictureBox4.Location = new System.Drawing.Point(52, 88);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(200, 195);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(198, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 35);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Paso 1: Escanear ID";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(283, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(40)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panelleft);
+            this.Controls.Add(this.Iinicio);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Iinicio.ResumeLayout(false);
+            this.Iinicio.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel Iinicio;
+        private System.Windows.Forms.Button btnDatos;
+        private System.Windows.Forms.Button btnKey;
+        private System.Windows.Forms.Panel panelleft;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Key Dispatcher/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+
+namespace Key_Dispatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+     
+        string devicname;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //search for the camera name it is independent from the camera start
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                devicname = filterInfo.Name;
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[0].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+           
+            captureDevice.Start();
+            
+            timer1.Start();
+
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = btnInicio.Height;
+            panelleft.Top = btnInicio.Top;
+            
+        }
+
+        private void btnKey_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = btnKey.Height;
+            panelleft.Top = btnKey.Top;
+
+        }
+
+        private void btnDatos_Click(object sender, EventArgs e)
+        {
+            panelleft.Height = btnDatos.Height;
+            panelleft.Top = btnDatos.Top;
+        }
+
+       
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+          
+
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureDevice.IsRunning)
+                captureDevice.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1!=null)
+            {
+                
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result;
+                if ((Bitmap)pictureBox1.Image != null) 
+                {
+                    int i;
+                    result = barcodeReader.Decode((Bitmap)pictureBox1.Image);
+                        
+                        i = 1;
+                        
+                           switch(i)
+                        {
+                            case 1 :
+                            if (result != null)
+                            {
+                                string resultado = result.ToString();
+                                timer1.Stop();
+
+
+                                pictureBox4.Image = Properties.Resources.tag;
+                                label1.Text = "Paso 2: Scanear Llaves";
+                                i++;
+                            }
+                                break;
+                            case 2:
+                            if (result != null)
+                            {
+                                timer1.Start();
+                                string resultado2 = result.ToString();
+                                i++;
+                            } 
+
+                                break;
+                        }
+                           
+
+                                
+                        
+                    
+                   
+
+                }
+                
+                 
+
+               
+            }
+        }
+
+        private void title(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
